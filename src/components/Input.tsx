fe4af3c21635e2f2883945ff9bf068a246b1bb19
Akino Archilles <nighthin@lidowin.com>
2022-06-11 02:51:27 +0700

    UI(components): refine input props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,18 @@ import { StyleSheet } from 'react-native';
 import { TextInput as PaperTextInput } from 'react-native-paper';
 import type { TextInputProps } from 'react-native-paper/lib/typescript/components/TextInput/TextInput';
 
-export default function TextInput(props: Partial<TextInputProps>) {
+type AdditionalTextInputProps = {
+  shadow: boolean;
+};
+
+export default function TextInput(
+  props: Partial<TextInputProps & AdditionalTextInputProps>
+) {
+  const isShadowPresent = props.shadow === undefined ? true : props.shadow;
+  const newStyles = [styles.container, props.style];
+  if (isShadowPresent) {
+    newStyles.push(styles.shadow);
+  }
   return (
     <PaperTextInput
       underlineColor='transparent'
@@ -13,25 +24,27 @@ export default function TextInput(props: Partial<TextInputProps>) {
       dense
       mode='flat'
       {...props}
-      style={[styles.container, props.style]}
+      style={newStyles}
     />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    elevation: 10,
     marginVertical: 5,
     width: '100%',
-    shadowOffset: {
-      height: 5,
-      width: 5,
-      maxHeight: 60,
-    },
     paddingVertical: 2,
     borderRadius: 5,
     shadowOpacity: 0.5,
     shadowRadius: 5,
     backgroundColor: 'white',
+    maxHeight: 60,
+  },
+  shadow: {
+    elevation: 10,
+    shadowOffset: {
+      height: 5,
+      width: 5,
+    },
   },
 });
