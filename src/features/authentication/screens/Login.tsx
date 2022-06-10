6fc2c0d89e3c7bf9d9a4187b5a28d74dc14d3f48
Akino Archilles <nighthin@lidowin.com>
2022-06-11 05:22:17 +0700

    UI(auth): login page styling

--- src/features/authentication/screens/Login.tsx
+++ src/features/authentication/screens/Login.tsx
@@ -5,40 +5,63 @@ import DeviceContants from '@constants/device';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { AuthContext } from '@@@/App';
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
-import { Button, Checkbox, Text } from 'react-native-paper';
+import { Checkbox, Text, ThemeProvider, useTheme } from 'react-native-paper';
 import { AuthenticationStackParamList } from '..';
+import CheckBoxStatus from '@interfaces/enums/CheckBoxStatus.enum';
+import Button from '@components/Button';
 
 const LoginScreen: React.FC = () => {
+  const theme = useTheme();
   const navigation =
     useNavigation<NativeStackNavigationProp<AuthenticationStackParamList>>();
 
   const { logIn }: any = React.useContext(AuthContext);
+  const [rememberMe, setRememberMe] = useState<CheckBoxStatus>(
+    CheckBoxStatus.UNCHECKED
+  );
+
+  const onPressRemember = () => {
+    if (rememberMe === CheckBoxStatus.CHECKED) {
+      setRememberMe(CheckBoxStatus.UNCHECKED);
+    } else {
+      setRememberMe(CheckBoxStatus.CHECKED);
+    }
+  };
 
   const onPressLogin = () => {
     logIn();
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: theme.colors.primary }]}>
       <Image
         source={require('@assets/onboarding/biometric-icon.png')}
         style={styles.logo}
       />
       <View style={styles.formLogin}>
-        <Input />
+        <Input placeholder='Alamat surel / NIK' shadow={false} />
         <SpaceBetween>
-          <Input style={{ flex: 1 }} />
-          <Button onPress={onPressLogin}>Masuk</Button>
+          <Input placeholder='Kata sandi' style={{ flex: 1 }} shadow={false} />
+          <Button style={styles.btnLogin} onPress={onPressLogin}>
+            Masuk
+          </Button>
         </SpaceBetween>
-        <SpaceBetween style={{ marginTop: 10 }}>
+        <SpaceBetween>
           <Row>
-            <Checkbox status='unchecked' />
-            <Text>aasdf</Text>
+            <Checkbox
+              status={rememberMe}
+              color={theme.colors.background}
+              uncheckedColor={theme.colors.background}
+              onPress={onPressRemember}
+            />
+            <Text style={{ color: theme.colors.background }}>Ingat saya</Text>
           </Row>
-          <Text onPress={() => navigation.navigate('ForgetPassword')}>
-            Lupa password?
+          <Text
+            style={[styles.forget, { color: theme.colors.background }]}
+            onPress={() => navigation.navigate('ForgetPassword')}>
+            Lupa kata sandi?
           </Text>
         </SpaceBetween>
       </View>
@@ -47,7 +70,9 @@ const LoginScreen: React.FC = () => {
           source={require('@assets/onboarding/biometric-icon.png')}
           style={styles.iconBiometric}
         />
-        <Text onPress={() => navigation.navigate('Register')}>
+        <Text
+          style={{ color: theme.colors.background }}
+          onPress={() => navigation.navigate('Register')}>
           Daftar akun baru
         </Text>
       </View>
@@ -62,7 +87,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginHorizontal: 10,
   },
   formLogin: {
     marginVertical: 40,
@@ -80,5 +104,9 @@ const styles = StyleSheet.create({
     height: 60,
     resizeMode: 'contain',
     marginBottom: 30,
+  },
+  btnLogin: { width: 'auto', marginLeft: 10 },
+  forget: {
+    textDecorationLine: 'underline',
   },
 });
