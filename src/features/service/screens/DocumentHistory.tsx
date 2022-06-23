8115fe7ff97ca156747171b69b898f491c72cffb
Akino Archilles <nighthin@lidowin.com>
2022-06-24 01:38:25 +0700

    UI(service): service history dummy list

--- src/features/service/screens/DocumentHistory.tsx
+++ src/features/service/screens/DocumentHistory.tsx
@@ -7,40 +7,32 @@ import { ScrollView } from 'react-native-gesture-handler';
 const DocumentHistoryScreen: React.FC = () => {
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      <DocumentItem
-        format='xlsx'
-        date='Senin, 11 Maret 2022'
-        title='Anggaran Pendapatan Belanja Desa 2022'
-      />
+      <DocumentItem format='xlsx' date='Senin, 11 Maret 2022' title='SKTM' />
+      <Separator width={'85%'} color={'lightgrey'} />
+      <DocumentItem format='xlsx' date='Senin, 11 Maret 2022' title='SKCK' />
       <Separator width={'85%'} color={'lightgrey'} />
       <DocumentItem
-        format='xlsx'
+        format='pdf'
         date='Senin, 11 Maret 2022'
-        title='Anggaran Pendapatan Belanja Desa 2022'
+        title='Surat Pengantar'
       />
       <Separator width={'85%'} color={'lightgrey'} />
       <DocumentItem
         format='pdf'
         date='Senin, 11 Maret 2022'
-        title='Anggaran Pendapatan Belanja Desa 2022'
+        title='Surat Pengesahan ANggaran Usaha 2022'
       />
       <Separator width={'85%'} color={'lightgrey'} />
       <DocumentItem
         format='pdf'
         date='Senin, 11 Maret 2022'
-        title='Anggaran Pendapatan Belanja Desa 2022'
-      />
-      <Separator width={'85%'} color={'lightgrey'} />
-      <DocumentItem
-        format='pdf'
-        date='Senin, 11 Maret 2022'
-        title='Anggaran Pendapatan Belanja Desa 2022'
+        title='Akte Pendirian Usaha'
       />
       <Separator width={'85%'} color={'lightgrey'} />
       <DocumentItem
         format='xlsx'
         date='Senin, 11 Maret 2022'
-        title='Anggaran Pendapatan Belanja Desa 2022'
+        title='Surat Pengaktifan Pajak'
       />
     </ScrollView>
   );
